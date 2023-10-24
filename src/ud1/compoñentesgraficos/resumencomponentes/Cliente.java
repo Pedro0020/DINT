b0890dc9335efc270a8c7d965e6f4a0d6dc437b4
Pedro <pedromaroga@gmail.com>
2023-10-24 21:28:31 +0200

    añado ejercicio repaso

---
+++ src/ud1/compoñentesgraficos/resumencomponentes/Cliente.java
@@ -0,0 +1,66 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package BBDD;
+
+/**
+ *
+ * @author node
+ */
+public class Cliente {
+    String Dni;
+    String Nombre;
+    String Direccion;
+    String Telefono;
+    
+
+    public Cliente(String Dni, String Nombre, String Direccion, String Telefono) {
+        this.Dni = Dni;
+        this.Nombre = Nombre;
+        this.Direccion = Direccion;
+        this.Telefono = Telefono;
+    }
+
+    public String getDni() {
+        return Dni;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public String getDireccion() {
+        return Direccion;
+    }
+
+    public String getTelefono() {
+        return Telefono;
+    }
+
+    public void setDni(String Dni) {
+        this.Dni = Dni;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public void setDireccion(String Direccion) {
+        this.Direccion = Direccion;
+    }
+
+    public void setTelefono(String Telefono) {
+        this.Telefono = Telefono;
+    }
+
+    @Override
+    public String toString() {
+        //return "Cliente{" + "Dni=" + Dni + ", Nombre=" + Nombre + ", Direccion=" + Direccion + ", Telefono=" + Telefono + '}';
+        return  Dni + " "  + Nombre ;
+    }
+    
+    
+    
+    
+}
